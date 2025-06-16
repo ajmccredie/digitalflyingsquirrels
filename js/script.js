2cabc8c93e1bbd4132369871ab5270222dd42a8c
AJ McCredie <127118760+ajmccredie@users.noreply.github.com>
2025-06-16 15:03:53 +0000

    Add some gallery pieces and swiper files

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@ const form = document.querySelector("form");
 form.addEventListener("submit", () => {
   alert("Thank you! Your message has been sent.");
 });
+
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+window.addEventListener('scroll', () => {
+  const header = document.querySelector('.sticky-header');
+  if (window.scrollY > 50) {
+    header.style.padding = '30px 0';
+  } else {
+    header.style.padding = '150px 0 100px';
+  }
+});
